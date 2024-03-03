125d1728cceaf8ae28595abf36e913c61149aebc
Lesia Derkach <108093435+Chercheuse@users.noreply.github.com>
2024-03-03 11:51:39 +0400

    Update index.js
    -

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-const checkbox = document.getElementById("switch-theme-checkbox")
-checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark")
-})
+const listItems = document.querySelectorAll('.nav-list-item');
+const currentPage = window.location.href;
+let link;
+
+// function itself should be imported to main.js
+export function changeNavBgColor() {
+  function removeAllCurrentPageClasses() {
+    listItems.forEach(item => item.classList.remove('current-page'));
+  }
+
+  function setCurrentPageClass(link) {
+    if (link && link.href === currentPage) {
+      link.closest('.nav-list-item').classList.add('current-page');
+    }
+  }
+
+  function clickHandler(event) {
+    removeAllCurrentPageClasses();
+    event.currentTarget.classList.add('current-page');
+  }
+
+  listItems.forEach(item => {
+    link = item.querySelector('.nav-link');
+    setCurrentPageClass(link);
+
+    item.addEventListener('click', clickHandler);
+  });
+}
+
+// function should be called in main.js
+changeNavBgColor();
